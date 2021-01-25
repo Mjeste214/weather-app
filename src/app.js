57010caa08edf9ef7395f68d82d8de52fe9d1b9a
majeste <sakaryafarukomer@gmail.com>
2021-01-26 01:21:43 +0300

    tr yazı

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicPath))
 
 app.get('', (req,res) => {
     res.render('index',{
-        title: 'Weather App',
+        title: 'Hava Durumu',
         name: 'OFS'
     });
 });
